9037f6b5edf656508be09934cb7b42b73ffbcfa4
Cédric Lepoittevin <cedric.lepoittevin@critizr.com>
2023-07-11 15:06:47 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('LibBundle');
         $rootNode = $treeBuilder->root('wh_lib');
 
         // Here you should define the parameters that are allowed to
